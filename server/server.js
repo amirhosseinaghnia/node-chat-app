d7fc9a88a6f593c8a93bf4afa750dab565c3c261
amirhossein <amirhossin1908@gmail.com>
2018-09-26 21:21:29 +0330

    Creatin basic express configs

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log(`server is up on ${prot}`);
+});
