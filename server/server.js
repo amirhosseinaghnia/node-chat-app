c7ed1f4cb3e1a55e4bbff82b03fc286df27b27b7
amirhossein <amirhossin1908@gmail.com>
2018-10-07 22:10:57 +0330

    Add geolocation support with geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -23,7 +23,11 @@ io.on('connection', (socket) => {
 
         io.emit('newMessage', generateMessage(message.from, message.text));
         // callback('From server');
-    })
+    });
+
+    socket.on('createLocationMessage', (location) => {
+        io.emit('newLocationMessage', generateLocationMessage('User', location.latitude, location.longitude));
+    });
 
     socket.on('disconnect', () => {
         console.log('client disconnected');
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,25 @@
 const chai = require('chai');
 const should = chai.should();
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-    it('should generate correct object', () => {
+    it('should generate correct message object', () => {
         var message = generateMessage('amir', 'hello from test suite');
         message.should.include({from: 'amir', text: 'hello from test suite'});
         message.createdAt.should.be.a('number');
     });
 });
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'amir';
+        var latitude = 4;
+        var longitude = 4;
+        var url = 'https://www.google.com/maps?q=4,4';
+        var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+        locationMessage.createdAt.should.be.a('number');
+        locationMessage.should.deep.include({from, url});
+    });
+})
